ba4d3b4ea291b0f3896e390713349493445b5d6a
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-11-04 13:23:54 -0300

    Use our remote for installation

--- install.sh
+++ install.sh
@@ -166,6 +166,8 @@ MKDIR=("/bin/mkdir" "-p")
 HOMEBREW_BREW_DEFAULT_GIT_REMOTE="https://github.com/Homebrew/brew"
 HOMEBREW_CORE_DEFAULT_GIT_REMOTE="https://github.com/Homebrew/homebrew-core"
 
+HOMEBREW_BREW_GIT_REMOTE="https://github.com/outstand/brew"
+
 # Use remote URLs of Homebrew repositories from environment if set.
 HOMEBREW_BREW_GIT_REMOTE="${HOMEBREW_BREW_GIT_REMOTE:-"${HOMEBREW_BREW_DEFAULT_GIT_REMOTE}"}"
 HOMEBREW_CORE_GIT_REMOTE="${HOMEBREW_CORE_GIT_REMOTE:-"${HOMEBREW_CORE_DEFAULT_GIT_REMOTE}"}"
